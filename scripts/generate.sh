2e6c042ba0a642167310c1032e857b7e0e43ef61
atli <atlisigurgeirsson@gmail.com>
2019-07-14 18:46:24 +0000

    deleted the scripts directory

--- scripts/generate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-#SBATCH --mem=64G
-#SBATCH --time=1800
-#SBATCH --gres=gpu:4
-
-~/envs/LAS/bin/python3 src/generate.py \
-    --name='char_lm_main'\
-    --config='./conf/malromur2017_default.yaml'
--- scripts/prepro.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-#SBATCH --mem=10G
-#SBATCH --time=1800
-
-~/envs/LAS/bin/python3 src/preprocess.py
-
